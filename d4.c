4eaac1c52abe97a460d5b07539bec9b1ec20c15e
4Rinzler4 <127138464+4Rinzler4@users.noreply.github.com>
2023-05-09 13:02:52 +0300

    d4.c

--- d4.c
+++ d4.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
-#include <math.h> 
+#include <math.h>//підключення бібліотек 
 int main(){
  double a = -M_PI/2;
  double b = M_PI/2;
  double dx = M_PI/20;
- double x, y;
+ double x, y;//оголошення змінних
 for (x = a; x <= b; x += dx){
-    y = 1 / tan(x) - 2 * sin(x);
-    printf("x = %f, y = %f\n", x, y);
+    /*y = 1 / tan(x) - 2 * sin(x);
+    printf("x = %.2f, y = %.2f\n", x, y);//виведення*/
+    if (x == 0)
+    {
+        printf("x = %.2f\t| Невизначено\n", y);
+    }
+    else{
+        y = 1 / tan(x) - 2 * sin(x);
+        printf("x = %.2f, y = %.2f\n", x, y);
+    }
 }
-return 0;
+return 0;//кінець програми
 }
